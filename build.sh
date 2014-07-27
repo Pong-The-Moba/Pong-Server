d2cd6b1b923460c01c5852c375c3dc8dc0230677
Chris <sihrc.c.lee@gmail.com>
2014-07-27 16:07:02 -0700

    ADD: build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+./gradlew executable-jar
+mv Server/build/libs/Server-execjar-1.0.jar jars/server.jar
+cd jars
+git pull origin master
+git commit -am "Update jars"
+git push origin master
+cd ..
+echo "1" | nc 192.168.1.72 1337 
